2dcc0e76dbe2afca4e883b2974c829388ec7ca33
iotjin <179608047@qq.com>
2020-06-22 17:12:38 +0800

    更新 UI

--- miniprogram/pages/demoList/listViewDemoList/listView/listViewGroup.js
+++ miniprogram/pages/demoList/listViewDemoList/listView/listViewGroup.js
@@ -10,20 +10,19 @@ Page({
           "AB_Remark": "备注",
           "AB_Date": "2011-11-11 15:38:16",
           "AB_FenLei": "支出",
-        },{
+        }, {
           "AB_LeiBie": "吃饭1",
           "AB_Money": "22",
           "AB_Remark": "备注2",
           "AB_Date": "2011-11-11 15:38:16",
           "AB_FenLei": "支出",
-        },{
+        }, {
           "AB_LeiBie": "红包1",
           "AB_Money": "33",
           "AB_Remark": "备注33",
           "AB_Date": "2011-11-11 15:38:16",
           "AB_FenLei": "收入",
-        },
-      ]
+        }, ]
       },
       {
         "AB_MonthDay": "29/06",
@@ -157,7 +156,72 @@ Page({
           "AB_FenLei": "支出",
         }]
       },
-    ]
+
+      {
+        "AB_MonthDay": "19/06",
+        "AB_ZhiChu": "1111",
+        "AB_ShouRu": "2222",
+        "AB_Bill": [{
+          "AB_LeiBie": "吃饭4",
+          "AB_Money": "44",
+          "AB_Remark": "",
+          "AB_Date": "2011-11-11 15:38:16",
+          "AB_FenLei": "支出",
+        }]
+      },
+      {
+        "AB_MonthDay": "19/06",
+        "AB_ZhiChu": "1111",
+        "AB_ShouRu": "2222",
+        "AB_Bill": [{
+          "AB_LeiBie": "吃饭4",
+          "AB_Money": "44",
+          "AB_Remark": "",
+          "AB_Date": "2011-11-11 15:38:16",
+          "AB_FenLei": "支出",
+        }]
+      },
+      {
+        "AB_MonthDay": "19/06",
+        "AB_ZhiChu": "1111",
+        "AB_ShouRu": "2222",
+        "AB_Bill": [{
+          "AB_LeiBie": "吃饭4",
+          "AB_Money": "44",
+          "AB_Remark": "",
+          "AB_Date": "2011-11-11 15:38:16",
+          "AB_FenLei": "支出",
+        }]
+      },
+      {
+        "AB_MonthDay": "19/06",
+        "AB_ZhiChu": "1111",
+        "AB_ShouRu": "2222",
+        "AB_Bill": [{
+          "AB_LeiBie": "吃饭4",
+          "AB_Money": "44",
+          "AB_Remark": "",
+          "AB_Date": "2011-11-11 15:38:16",
+          "AB_FenLei": "支出",
+        }]
+      },
+      {
+        "AB_MonthDay": "19/06",
+        "AB_ZhiChu": "1111",
+        "AB_ShouRu": "2222",
+        "AB_Bill": [{
+          "AB_LeiBie": "吃饭4",
+          "AB_Money": "44",
+          "AB_Remark": "",
+          "AB_Date": "2011-11-11 15:38:16",
+          "AB_FenLei": "支出",
+        }]
+      },
+
+
+
+    ],
+    Time:0,
 
   },
   onShow: function () {
@@ -165,8 +229,63 @@ Page({
       title: '分组列表'
     })
   },
-  onPageScroll: function (res) {
-    console.log(res.scrollTop);
-  }
+  onPageScroll: function (e) {
+    // console.log(e.scrollTop);
+
+    // let index = e.scrollTop / 71
+    // index = parseInt(index);
+    // console.log(index);
+    // let data = this.data.dataArr[index]
+
+    let topHeight = 120
+    var that = this;
+    var query = wx.createSelectorQuery().in(that);
+    query.selectViewport().scrollOffset()
+    query.selectAll("#header").boundingClientRect();
+    // query.select("#header").boundingClientRect();
+    query.exec(function (res) {
+      // console.log(res);
+      let time = ''
+      var resultIndex = res[1].findIndex((v) => {
+        return v.top === topHeight;
+      });
+      if (resultIndex == -1) {
+        let tempArr = []
+        for (let index = 0; index < res[1].length; index++) {
+          let data = res[1][index];
+          tempArr.push(data.top)
+        }
+        let tempIndex = that.getClosestValueIndex(tempArr, topHeight)
+        // console.log(tempIndex);
+        time = res[1][tempIndex].dataset.selectdata
+      } else {
+        time = res[1][resultIndex].dataset.selectdata
+      }
+      console.log(time);
+
+      that.setData({
+        Time: time,
+      });
+    });
+
+  },
+
+  getClosestValueIndex: function (arr, num) {
+    var newArr = [];
+    arr.map(function (x) {
+      // 对数组各个数值求差值
+      newArr.push(Math.abs(x - num));
+    });
+    // 求最小值的索引
+    var index = newArr.indexOf(Math.min.apply(null, newArr));
+    // return arr[index];
+    return index;
+  },
+
+  ClickCell() {
+    console.log("点击cell");
+
+  },
+
 
 })
