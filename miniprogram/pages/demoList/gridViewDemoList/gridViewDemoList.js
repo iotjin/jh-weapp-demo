2b404004040a8317f55ae5ba44460c392a474a09
iotjin <179608047@qq.com>
2021-01-19 18:16:12 +0800

    update UI

--- miniprogram/pages/demoList/gridViewDemoList/gridViewDemoList.js
+++ miniprogram/pages/demoList/gridViewDemoList/gridViewDemoList.js
@@ -5,15 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    dataArr: [
-      {
+    dataArr: [{
         text: 'GridView1 - 动态数据',
         pathUrl: '/pages/demoList/gridViewDemoList/gridView/gridView1'
       },
-       {
+      {
         text: 'GridView2 - 固定头尾',
         pathUrl: '/pages/demoList/gridViewDemoList/gridView/gridView2'
-      }, 
+      },
       {
         text: 'GridView3 - 固定头尾',
         pathUrl: '/pages/demoList/gridViewDemoList/gridView/gridView3'
@@ -21,6 +20,10 @@ Page({
       {
         text: 'GridView4 - 多列',
         pathUrl: '/pages/demoList/gridViewDemoList/gridView/gridView4'
+      },
+      {
+        text: 'GridView5 - 功能列表',
+        pathUrl: '/pages/demoList/gridViewDemoList/gridView/function'
       },
     ]
   },
